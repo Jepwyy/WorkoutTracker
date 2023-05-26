5c1f684d95eaae0d15e1768b69cfca2e5c1ed06d
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-05-26 15:28:55 +0800

    view/edit profile, exit

--- main_page.py
+++ main_page.py
@@ -1,17 +1,33 @@
 from tkinter import *
 from tkinter import messagebox
+from profile_page import ProfilePage
+from db_connector import connect
 
 class MainPage:
     def __init__(self, name):
+        self.name = name
         self.main_page = Tk()  # Create the main page window
         self.main_page.title("Workout Tracker - Main Page")
 
-        welcome_label = Label(self.main_page, text="Welcome, " + name + "!" )
-        welcome_label.pack()
+        self.welcome_label = Label(self.main_page, text="Welcome, " + name + "!")
+        self.welcome_label.pack()
+
+        # Retrieve user's present weight
+        db = connect()
+        cursor = db.cursor()
+        query = "SELECT * FROM users WHERE name = %s"
+        values = (self.name,)
+
+        cursor.execute(query, values)
+        user = cursor.fetchone()
+        present_weight = user[4] if user else "N/A"
+
+        self.present_weight_label = Label(self.main_page, text="\nPresent Weight: " + str(present_weight) + " Kg")
+        self.present_weight_label.pack()
 
         # Add your main page content here
         # Button for viewing profile
-        view_profile_button = Button(self.main_page, text="View Profile")
+        view_profile_button = Button(self.main_page, text="View Profile", command=self.view_profile)
         view_profile_button.pack()
 
         # Button for adding exercise
@@ -41,6 +57,30 @@ class MainPage:
         # Button for generating total summary reports
         summary_reports_button = Button(self.main_page, text="Total Summary Reports")
         summary_reports_button.pack()
-        # ...
+
+        # Button for exiting the application
+        exit_button = Button(self.main_page, text="Exit", command=self.exit_application)
+        exit_button.pack()
 
         self.main_page.mainloop()
+
+    def view_profile(self):
+        db = connect()
+        cursor = db.cursor()
+        query = "SELECT * FROM users WHERE name = %s"
+        values = (self.name,)
+
+        cursor.execute(query, values)
+        user = cursor.fetchone()
+
+        if user:
+            profile_window = ProfilePage(user, self.update_main_page)
+        else:
+            messagebox.showerror("Error", "User Doesn't Exist")
+
+    def update_main_page(self):
+        self.main_page.destroy()
+        self.__init__(self.name)
+
+    def exit_application(self):
+        self.main_page.destroy()
---
+++ profile_page.py
@@ -0,0 +1,86 @@
+from tkinter import *
+from tkinter import messagebox
+from db_connector import connect
+
+class ProfilePage:
+    def __init__(self, user, update_callback):
+        self.user = user
+        self.update_callback = update_callback
+
+        self.profile_page = Toplevel()
+        self.profile_page.title("Profile")
+        self.profile_page.geometry("300x200")
+
+        self.name_label = Label(self.profile_page, text="Name: " + user[1])
+        self.name_label.pack()
+
+        self.username_label = Label(self.profile_page, text="Username: " + user[2])
+        self.username_label.pack()
+
+        self.weight_label = Label(self.profile_page, text="Weight: " + str(user[4]))
+        self.weight_label.pack()
+
+        self.edit_button = Button(self.profile_page, text="Edit Profile", command=self.open_edit_window)
+        self.edit_button.pack()
+
+        # Initialize edit_window as None
+        self.edit_window = None
+
+    def open_edit_window(self):
+        # Check if edit_window is already open
+        if self.edit_window is not None:
+            return
+
+        self.edit_window = Toplevel(self.profile_page)
+        self.edit_window.title("Edit Profile")
+        self.edit_window.geometry("300x200")
+
+        self.name_label = Label(self.edit_window, text="Name")
+        self.name_label.pack()
+        self.name_entry = Entry(self.edit_window)
+        self.name_entry.insert(0, self.user[1])  # Populate the entry field with the current name
+        self.name_entry.pack()
+
+        self.username_label = Label(self.edit_window, text="Username")
+        self.username_label.pack()
+        self.username_entry = Entry(self.edit_window)
+        self.username_entry.insert(0, self.user[2])  # Populate the entry field with the current username
+        self.username_entry.pack()
+
+        self.weight_label = Label(self.edit_window, text="Weight")
+        self.weight_label.pack()
+        self.weight_entry = Entry(self.edit_window)
+        self.weight_entry.insert(0, str(self.user[4]))  # Populate the entry field with the current weight
+        self.weight_entry.pack()
+
+        save_button = Button(self.edit_window, text="Save Changes", command=self.save_changes)
+        save_button.pack()
+
+    def save_changes(self):
+        new_name = self.name_entry.get()
+        new_username = self.username_entry.get()
+        new_weight = float(self.weight_entry.get())
+
+        db = connect()
+        cursor = db.cursor()
+        query = "UPDATE users SET name = %s, username = %s, weight = %s WHERE id = %s"
+        values = (new_name, new_username, new_weight, self.user[0])
+
+        cursor.execute(query, values)
+        db.commit()
+
+        messagebox.showinfo("Success", "Profile updated successfully.")
+        self.user = (self.user[0], new_name, new_username, self.user[3], new_weight)
+        self.update_labels()
+
+        # Close the edit window and profile page
+        self.edit_window.destroy()
+        self.profile_page.destroy()
+
+        # Call the update callback
+        self.update_callback()
+
+    def update_labels(self):
+        self.name_label.config(text="Name: " + self.user[1])
+        self.username_label.config(text="Username: " + self.user[2])
+        self.weight_label.config(text="Weight: " + str(self.user[4]))
